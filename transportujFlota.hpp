7e16787e90328bc2c7c40351306799675b8f1511
Andrzej Graska <graskaandrzej@gmail.com>
2024-10-30 22:00:01 +0100

    Wspaniala zejsciowka :)

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,25 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int ilosc_towaru;
+    unsigned int zaglowce;
+    zaglowce=0;
+    ilosc_towaru=0;
+
+    Stocznia stocznia{};
+    Statek*  s1;
+
+    while (ilosc_towaru < towar) {
+        s1 = stocznia();
+        if (dynamic_cast< Zaglowiec* >(s1) != nullptr) {
+            zaglowce++;
+        }
+
+        ilosc_towaru=ilosc_towaru+s1->transportuj();
+        // s1->transportuj();
+
+        delete s1;
+    }
+
+    return zaglowce;
 }
